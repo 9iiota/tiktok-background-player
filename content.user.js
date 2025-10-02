37bc2e8fd23ae61d01ecb296bb33468d1edf83b8
Burak Kilic <crazyshocklight@gmail.com>
2025-10-02 15:45:17 +0200

    Refactor user script to directly require content.js and remove unnecessary fetch logic

--- content.user.js
+++ content.user.js
@@ -6,17 +6,5 @@
 // @match        *://*.tiktok.com/*
 // @run-at       document-start
 // @grant        none
+// @require https://raw.githubusercontent.com/9iiota/tiktok-background-player/main/content.js
 // ==/UserScript==
-
-(function ()
-{
-    'use strict';
-
-    (async () =>
-    {
-        const response = await fetch('https://github.com/9iiota/tiktok-background-player/content.js'); // TODO: change URL to your content.js location
-        const code = await response.text();
-        console.log('Fetched code:', code); // Log the fetched code for debugging
-        // eval(code);
-    })();
-})();
